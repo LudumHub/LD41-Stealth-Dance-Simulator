9873f2d6836b0e63447fc975f2fedc62da39f3ad
Linver <mehael@mail.ru>
2018-04-24 08:57:22 +1000

    Add main menu.

---
+++ Assets/LevelDesign/MenuClickWaiter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuClickWaiter : MonoBehaviour {
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+            Fade.instance.LoadNextLevel();
+	}
+}
